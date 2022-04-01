ee20ce73fe6d98828eddb3ea4241aeb82406e57e
Martin Kean <martinkean15@gmail.com>
2022-04-01 14:11:31 +1300

    Added structured data

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
 const { DateTime } = require("luxon");
+const markdownIt = require("markdown-it");
+const markdownItAnchor = require("markdown-it-anchor");
+const schema = require("@quasibit/eleventy-plugin-schema");
 
 module.exports = function(eleventyConfig) {
 
@@ -6,9 +9,18 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./src/images');
     eleventyConfig.addPassthroughCopy('./src/admin');
 
+    // eleventyConfig.addPlugin(pluginRss);
+    // eleventyConfig.addPlugin(pluginSyntaxHighlight);
+    // eleventyConfig.addPlugin(pluginNavigation);
+    eleventyConfig.addPlugin(schema);
+
     eleventyConfig.addFilter("postDate", (dateObj) => {
         return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
-    })
+        })
+
+    eleventyConfig.addFilter('iso8601', (dateObj) => {
+        return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toISO()
+      })
 
     return {
         dir: {
